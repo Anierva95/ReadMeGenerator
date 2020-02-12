0d7697037f8f10ed03262884bd0e514aba0c8be2
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-02-12 18:49:19 -0500

    Successfully stored userAvatar and got query correct

--- index.js
+++ index.js
@@ -9,23 +9,11 @@ inquirer
       message: "Please enter your Github Username.",
       name: "username"
     }
-  ])
-
-  .then(function({ username })) {
-    const queryURL = `https://api.github.com/users/${username}/repos?per_page=100`;
+  ]).then(function({ username }) {
+    const queryURL = `https://api.github.com/users/${username}`;
     
     axios.get(queryURL).then(function(res) {
-
-    console.log(res.data);
-    console.log(res.data.url);
+    console.log("Here is my data!!!")
     console.log(res.data.avatar_url);
-  })
-
-
-  //.then(function(response) {
-
-    //fs.writeFile('README.md', JSON.stringify(response), function(error) {
-      //  return error;
-    //});
-   // }
- // );
+    const userAvatar = res.data.avatar_url;
+  })})

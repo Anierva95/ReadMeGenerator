e12bbcf20ddcb0dcfe5dbe930aaf2f5182eff2dd
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-02-13 15:13:41 -0500

    Finally done

--- index.js
+++ index.js
@@ -33,8 +33,18 @@ inquirer
       type: "input",
       message: "Please provide who has contributed to this project",
       name: "credits"
+    },
+    {
+      type: "input",
+      message: "What kind of tests have you performed for this application?",
+      name: "tests"
+    },
+    {
+      type: "input",
+      message: "Provide an E-mail for those with questions about your app.",
+      name: "email"
     }
-  ]).then(function({ username, credits, usage, installation, description, title }) {
+  ]).then(function({ username, credits, usage, installation, description, title, tests, email }) {
 
     const queryURL = `https://api.github.com/users/${username}`;
 
@@ -45,9 +55,11 @@ inquirer
     usage = usage + "\n\n";
     installation = installation + "\n\n";
     description = description + "\n\n";
-    title = title + "\n\n"
+    title = title + "\n\n";
+    tests = tests + "\n\n";
+    email = email + "\n\n";
 
-    const data = `# Title \n\n ${title} ## Description  \n\n ${description} ## Table of Contents \n\n - [Installation](Intallation)\n\n - [Usage](Usage) \n\n - [Credits](Credits) \n\n ## Installation \n\n ${installation} ## Usage \n\n ${usage} ## Credits \n\n ${credits} ${userAvatar}`;
+    const data = `# Title \n\n ${title} ## Description  \n\n ${description} ## Table of Contents \n\n - [Installation](Intallation)\n\n - [Usage](Usage) \n\n - [Credits](Credits) \n\n - [Tests](Tests) \n\n - [Contact](Contact) \n\n ## Installation \n\n ${installation} ## Usage \n\n ${usage} ## Credits \n\n ${credits} ## Tests \n\n ${tests} ## Contact \n\n ${email} ![UserImage](${userAvatar})`;
 
     fs.writeFile("ReadMe.md", data, function(err) {
       if (err)  console.log(err);
